016d5ae7359d7b08731684a5abbcd415ce586194
Dennis O'Keeffe <hello@dennisokeeffe.com>
2022-03-18 10:22:00 +1100

    feat: first attempt at post

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+gem 'rspec', group: :test
+gem 'simplecov', require: false, group: :test
+gem 'simplecov-lcov', require: false, group: :test
+gem 'pry'
---
+++ spec/math_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'contrived_math'
+
+RSpec.describe Math do
+  describe '#add' do
+    it 'adds two numbers' do
+      expect(ContrivedMath.add(1, 2)).to eq(3)
+    end
+  end
+
+  describe '#subtract' do
+    it 'subtracts two numbers' do
+      expect(ContrivedMath.subtract(2, 1)).to eq(1)
+    end
+  end
+end
